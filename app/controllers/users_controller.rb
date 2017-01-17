097e98b18ef3f23be7dc10c8611dcd127e2bcebb
mrunal-selokar <mrunal_selokar@outlook.com>
2017-01-17 18:17:05 +0530

    invalid signup test added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
   end
   private
     def user_params
-      params.require(:usesr).permit(:name, :email, :password, :password_confirmation)
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
 
 end
