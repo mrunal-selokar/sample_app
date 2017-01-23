f12845349502050f01323a038d2d304d507fce65
mrunal-selokar <mrunal_selokar@outlook.com>
2017-01-23 16:53:58 +0530

    successful logout added

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -6,11 +6,16 @@ module SessionsHelper
 
 	#returns the current logged in user(if any)
 	def current_user
-		current_user||=User.find_by(id:session[:user_id])
+		@current_user||=User.find_by(id:session[:user_id])
 	end
 
 	#returns true if the user is logged in, false otherwise
 	def logged_in?
 		!current_user.nil?
+	end
+
+	def log_out
+		session.delete(:user_id)
+		@current_user=nil
 	end
 end
