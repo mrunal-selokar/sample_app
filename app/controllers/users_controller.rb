e6c7a96dbe249b2c49f997e0832f725933cb90fd
mrunal-selokar <mrunal_selokar@outlook.com>
2017-01-24 11:05:48 +0530

    Form duplication removed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,11 @@ class UsersController < ApplicationController
   def show
   	    @user = User.find(params[:id])
   end
+  
   def new
   	@user = User.new
   end
+  
   def create
   	@user=User.new(user_params)
   	if @user.save
@@ -15,6 +17,11 @@ class UsersController < ApplicationController
   		render 'new'
   	end
   end
+
+  def edit
+    @user=User.find(params[:id])
+  end
+
   private
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
