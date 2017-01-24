2c87476bd9077f5a1b78df984f6d2e02c014687b
mrunal-selokar <mrunal_selokar@outlook.com>
2017-01-24 19:03:26 +0530

    Authenticated method generalized

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,9 +30,10 @@ end
   end
 
   #returns true if given token matches the digest
-  def authenticated?(remember_token)
-    return false if remember_digest.nil?
-    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  def authenticated?(attribute, token)
+    digest= send("#{attribute}_digest")
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
   end
 
   #forgets a user
