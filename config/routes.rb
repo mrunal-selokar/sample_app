026527bc39acffc14c8079a23c0e279ed00ed5c9
mrunal-selokar <mrunal_selokar@outlook.com>
2017-01-24 15:49:07 +0530

    Activation attribute added

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
   post    '/login',    to: 'sessions#create'
   delete  '/logout',   to: 'sessions#destroy'
   resources :users
+  resources :account_activations, only: [:edit]
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
