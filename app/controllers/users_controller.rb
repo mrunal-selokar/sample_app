8e37cce72fe6aa38c1c5e484c563f448c65e535f
mrunal-selokar <mrunal_selokar@outlook.com>
2017-01-24 13:10:16 +0530

    users index added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [:index,:edit, :update]
   before_action :correct_user, only: [:edit, :update]
   def show
   	    @user = User.find(params[:id])
@@ -22,6 +22,10 @@ class UsersController < ApplicationController
 
   def edit
     @user=User.find(params[:id])
+  end
+
+  def index
+    @users=User.all
   end
 
   def update
