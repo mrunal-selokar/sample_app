31d2fb723e10e357477b0191b2fd6b2a73ff71e1
mrunal-selokar <mrunal_selokar@outlook.com>
2017-01-24 12:49:09 +0530

    forwarding non logged-in users

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -15,7 +15,7 @@ module SessionsHelper
 	def current_user?(user)
 		user==current_user
 	end
-	
+
 	#returns the current logged in user(if any)
 	def current_user
 		if (user_id=session[:user_id])
@@ -46,5 +46,16 @@ module SessionsHelper
 		forget(current_user)
 		session.delete(:user_id)
 		@current_user=nil
+	end
+
+	#redirects to stored location(or to default)
+	def redirect_back_or(default)
+		redirect_to(session[:forwarding_url]||default)
+		session.delete(:forwarding_url)
+	end
+
+	#stores the url trying to be accessed
+	def store_location
+		session[:forwarding_url]=request.original_url if request.get?
 	end
 end
