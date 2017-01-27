44ed8490e54a10838e010e684a20fefad21fc8b1
mrunal-selokar <mrunal_selokar@outlook.com>
2017-01-27 17:16:00 +0530

    microposts added to user profile- unstyled

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,11 @@ class UsersController < ApplicationController
   def new
   	@user = User.new
   end
+
+  def show
+    @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
+  end
   
   def create
   	@user=User.new(user_params)
